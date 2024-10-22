60c612ef93204cc6463867c977f1678eacb8b4db
SebastianIRodriguez <seba.rodriguez.994@gmail.com>
2024-10-22 17:55:53 -0300

    Determinamos el tiempo en alto de cada pwm a partir del sextante, ta, tb y t0 , y de los vectores fijos correspondientes
    Co-authored-by: gabo-gd@hotmail.com

--- vector_rotante_v0/main.c
+++ vector_rotante_v0/main.c
@@ -13,12 +13,68 @@ unsigned int frecuencia = 50;
 int interrupcion()
 {
     static float fase = 0;
+    float E = 1;
+    float V_ref = 1;  //modulo?
 
     float periodo = 1 / frecuencia;
     float Ts = 0.0001; // Siempre que Fs sea 10 kHz
+
     fase += Ts * 2 * M_PI / periodo;
+    fase = fase % (2 * M_PI);
+
     float Vx = modulo * cos(fase);
     float Vy = modulo * sin(fase);
+
+    float fase_rel = fase % (M_PI / 3);
+    int sext = (int) ceil(fase / (M_PI / 3));
+
+    float ta, tb, t0;
+    ta = sqrt(3) * V_ref / E * Ts * sin(M_PI / 3 - fase_rel);
+    tb = sqrt(3) * V_ref / E * Ts * sin(fase_rel);
+    t0 = Ts - ta - tb;
+    
+    float t_pwm1, t_pwm2, t_pwm3;
+    switch (sext)
+    {
+    case 1:
+        t_pwm1 = ta + tb + t0 / 2;
+        t_pwm2 = tb + t0 / 2;
+        t_pwm3 = t0 / 2;
+        break;
+    
+    case 2:
+        t_pwm1 = tb + t0 / 2;
+        t_pwm2 = ta + tb + t0 / 2;
+        t_pwm3 = t0 / 2;
+        break;
+    
+    case 3:
+        t_pwm1 = t0 / 2;
+        t_pwm2 = ta + tb + t0 / 2;
+        t_pwm1 = tb + t0 / 2;
+        break;
+
+    case 4:
+        t_pwm1 = t0 / 2;
+        t_pwm2 = tb + t0 / 2;
+        t_pwm1 = ta + tb + t0 / 2;
+        break;
+
+    case 5:
+        t_pwm1 = tb + t0 / 2;
+        t_pwm2 = t0 / 2;
+        t_pwm1 = ta + tb + t0 / 2;
+        break;
+
+    case 6:
+        t_pwm1 = ta + tb + t0 / 2;
+        t_pwm2 = t0 / 2;
+        t_pwm1 = tb + t0 / 2;
+        break;
+    
+    default:
+        break;
+    }
 
     return 0;
 }
